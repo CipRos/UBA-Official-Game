9284b5bd8d61785dfc0c8e27e7b25bb08e144a61
CipRos <42282352+CipRos@users.noreply.github.com>
2019-08-07 16:17:18 +0300

    Add files via upload

---
+++ Updater.bat
@@ -0,0 +1,7 @@
+@echo off
+cd C:\Users\%username%\Desktop
+git clone https:\\github.com\CipRos\UBA-Official-Game
+echo Update Complete! Don't delete this folder! It will automaticly be removed when you open the game!
+pause
+start C:\Users\%username%\Desktop\UBA-Official-Game\UBA_V1.exe
+exit
